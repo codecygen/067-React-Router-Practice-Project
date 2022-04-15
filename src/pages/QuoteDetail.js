ad997736a5bc258c5de6e2abad49ab3a222b3faa
gingercook <vahitaras@gmail.com>
2022-04-15 18:08:54 -0400

    quote detail page added

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -2,15 +2,26 @@ import React from 'react';
 
 import { Route, useParams } from 'react-router-dom';
 
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+    { id: 'q1', author: 'Aras', text: 'I am learning React Route.' },
+    { id: 'q2', author: 'Joe', text: 'Front End is fun!' },
+];
 
 const QuoteDetail = () => {
     const params = useParams();
 
+    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+
+    if (!quote) {
+        return <p>No Quote Found!</p>
+    }
+
     return (
         <>
-            <div>QuoteDetail</div>
-            <p>{params.quoteId}</p>
+            <HighlightedQuote text={quote.text} author={quote.author} />
 
             <Route path={`/quotes/${params.quoteId}/comments`}>
                 <Comments />
