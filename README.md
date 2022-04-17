8cb8c6b6aacb2d6a3f087a57203555b441d5c44f
gingercook <vahitaras@gmail.com>
2022-04-16 21:13:25 -0400

    sorting is worked on

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 
 ## React-Router-Prevent-User-to-Lose-Entered-Data-By-Changing-Page 
 - [src/components/quotes/QuoteForm.js](https://github.com/codecygen/067-React-Router-Practice-Project/blob/main/src/components/quotes/QuoteForm.js)
+
+## React-Router-Query-Parameters
+[src/components/quotes/QuoteList.js](https://github.com/codecygen/067-React-Router-Practice-Project/blob/main/src/components/quotes/QuoteList.js)
--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,11 +1,40 @@
 import { Fragment } from 'react';
 
+// React-Router-Query-Parameters
+import { useHistory, useLocation } from 'react-router-dom';
+
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+
 const QuoteList = (props) => {
+  // React-Router-Query-Parameters
+  // Used to manipulate page after a button click
+  const history = useHistory();
+
+  // React-Router-Query-Parameters
+  // Gets the current page address
+  const location = useLocation();
+  
+  // React-Router-Query-Parameters
+  // location.search gives "?sort=asc"
+  // URLSearchParams is a default Javascript constructor
+  const queryParams = new URLSearchParams(location.search);
+
+  const isSortingAscending = queryParams.get('sort')
+
+  // React-Router-Query-Parameters
+  const changeSortingHandler = () => {
+    history.push('/quotes?sort=asc');
+  };
+
   return (
     <Fragment>
+      {/* React-Router-Query-Parameters */}
+      <div className={classes.sorting}>
+        {/* React-Router-Query-Parameters */}
+        <button onClick={changeSortingHandler}>Sort Ascending</button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
